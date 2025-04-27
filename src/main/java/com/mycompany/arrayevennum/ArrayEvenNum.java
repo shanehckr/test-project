c0660b24ec9abe4f603b2a8079b747bae4d83b2e
shanedelapaz <shanedelapaz0@gmail.com>
2025-04-27 19:21:10 +0800

    change text commit.

---
+++ src/main/java/com/mycompany/arrayevennum/ArrayEvenNum.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arrayevennum;
+
+/**
+ *
+ * @author kearl
+ */
+public class ArrayEvenNum {
+
+    public static void main(String[] args) {
+        
+        
+        int[] arr = {1,2,3,4,5};
+        System.out.print("Even number: ");
+        for (int i: arr) {
+            if (i % 2 == 0) System.out.print(i + " ");
+            
+            System.out.println("test");
+        }
+        
+    }
+}
