c7d131c9389ac0de722674ecfc165bd320091c0b
shanehckr <kearldelapaz@gmail.com>
2025-04-27 22:33:22 +0800

    final commit kasi pagod na ako.

--- src/main/java/com/mycompany/arrayevennum/ArrayEvenNum.java
+++ src/main/java/com/mycompany/arrayevennum/ArrayEvenNum.java
@@ -12,18 +12,14 @@ public class ArrayEvenNum {
 
     public static void main(String[] args) {
         
+        System.out.println("last na talaga promiseee kasi iiyak na ako");
         
         int[] arr = {1,2,3,4,5};
         System.out.print("Even number: ");
         for (int i: arr) {
             if (i % 2 == 0) System.out.print(i + " ");
             
-            System.out.println("test");
-            System.out.println("testttttt");
-            System.out.println("branch test");
-            System.out.println("text please");
-            System.out.println("hahaahaa");
-            System.out.println("okayyyy");
+            
         }
         
     }
